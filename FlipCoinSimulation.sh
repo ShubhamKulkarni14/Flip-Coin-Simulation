5c0e4f4cb27c24099f7992ba147b7745a8cd59c8
ShubhamKulkarni14 <shubhuk14@gmail.com>
2020-11-23 22:13:37 +0530

    Added :Using For Loop For Different Results

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -x
 
-flip=$(($(($RANDOM%10))%2))
-if [ $flip -eq 1 ]
-then
-    echo "HEADS"
-else
-    echo "TAILS"
-fi
+h_c=0
+l_c=0
+
+for ((i=1;i<10;i++))
+do
+	flip=$(($(($RANDOM%10))%2))
+	if [ $flip -eq 1 ]; then
+    		echo "HEADS"
+        	h_c=$(($h_c+1))
+	else
+    		echo "TAILS"
+        	l_c=$(($l_c+1))
+	fi
+done
+echo "HEAD COUNT : " $h_c
+echo "TAIL COUNT : " $l_c
